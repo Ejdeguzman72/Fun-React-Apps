a242c0c0ecc965f80235f3b96f05ea25c1af7a6c
Ejdeguzman72 <ejdeguzman72@gmail.com>
2020-03-27 23:51:54 -0500

    initial commit

---
+++ src/component/to-do-list-component.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import List from '../component/list';
+
+export default class ToDoListComponent extends React.Component {
+    constructor(props)  {
+        super(props);
+        this.state = {
+            term: '',
+            items: []
+        };
+    }
+
+    onChange = (event) => {
+        this.setState({term:  event.target.value});
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            term:'',
+            items: [...this.state.items, this.state.term]
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="App" onSubmit={this.onSubmit}>
+                    <input value={this.state.term}  onChange={this.onChange} />
+                    <button>Submit</button>
+                </form>
+                <List items={this.state.items} />
+            </div>
+        )
+    }
+}
